8f561fd1f377a592baa2595a5c123c171e40755f
Leidy-Lesmes <70681138+Leidy-Lesmes@users.noreply.github.com>
2024-02-24 17:25:12 -0500

    Actualización del estado del carro

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+process.env.SERVER_IP = '127.0.0.1'; 
+
+
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
@@ -14,13 +17,15 @@ let parkedCars = [];
 
 // Registrar el ingreso a un parqueadero de un carro
 app.post('/cars', (req, res) => {
-    const { license_plate, color, state, image_url } = req.body;
+    const { license_plate, color, image_url } = req.body;
     const entryTime = new Date();
+    const state = "Activo"; // Establecer el estado como activo
     const car = { license_plate, color, entryTime, state, image_url };
     parkedCars.push(car);
     console.log('Car parked:', car);
     res.status(201).json(car);
 });
+
 
 // Listar los vehículos registrados
 app.get('/cars', (req, res) => {
@@ -34,14 +39,15 @@ app.listen(PORT, () => {
 
 // Retirar el carro usando la placa
 app.patch('/cars', (req, res) => {
-    const { license_plate, state } = req.body;
+    const { license_plate } = req.body;
     const index = parkedCars.findIndex(car => car.license_plate === license_plate);
     if (index !== -1) {
-        parkedCars[index].state = state; // Asignar el nuevo estado al carro encontrado
-        const updatedCar = parkedCars[index]; // El carro actualizado
+        parkedCars[index].state = "Retirado"; // Cambiar el estado a "retirado"
+        const updatedCar = parkedCars[index];
         console.log('Car state updated:', updatedCar);
         res.json(updatedCar);
     } else {
         res.status(404).json({ error: 'Car not found' });
     }
 });
+
