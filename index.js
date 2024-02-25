2a7fe538593bb12e7bdde4bec3d6fd98e2a7678d
Leidy-Lesmes <70681138+Leidy-Lesmes@users.noreply.github.com>
2024-02-25 10:43:41 -0500

    Carga de imagen a imgur

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ process.env.SERVER_IP = '127.0.0.1';
 
 
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const morgan = require('morgan');
 const cors = require('cors');
+const axios = require('axios');
+const FormData = require('form-data');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,18 +17,71 @@ app.use(express.json());
 app.use(morgan('combined'));
 app.use(cors());
 
+// Configuración de express-fileupload
+app.use(fileUpload());
+
+// Resto de tu código
+
+
 let parkedCars = [];
 
 // Registrar el ingreso a un parqueadero de un carro
-app.post('/cars', (req, res) => {
-    const { license_plate, color, image_url } = req.body;
+app.post('/cars', async (req, res) => {
+    const { license_plate, color } = req.body;
     const entryTime = new Date();
-    const state = "Activo"; // Establecer el estado como activo
-    const car = { license_plate, color, entryTime, state, image_url };
-    parkedCars.push(car);
-    console.log('Car parked:', car);
-    res.status(201).json(car);
+    const state = "Activo"; 
+
+    try {
+        if (!req.files || Object.keys(req.files).length === 0) {
+            console.log(req.files);
+            return res.status(400).send('No se encontró ningún archivo.');
+        }
+
+        const file = req.files.image_path;
+        const fileName = file.name;
+
+        // Guardar el archivo en el servidor
+        file.mv(`${__dirname}/uploads/${fileName}`, async function(err) {
+            console.log ('El archivo se guardo en uploads: ' + fileName);
+            if (err) {
+                console.error('Error al guardar el archivo:', err);
+                return res.status(500).json({ error: 'Error interno del servidor al guardar el archivo.' });
+            }
+
+            // Subir la imagen al servidor de Imgur
+            const formData = new FormData();
+            formData.append('image', fs.createReadStream(`${__dirname}/uploads/${fileName}`));
+            formData.append('type', 'image');
+            formData.append('title', 'Simple upload');
+            formData.append('description', 'This is a simple image upload in Imgur');
+
+            const imgurResponse = await axios.post('https://api.imgur.com/3/image', formData, {
+                headers: {
+                    Authorization: 'Client-ID ae2537bec814728', // Reemplaza YOUR_CLIENT_ID con tu ID de cliente de Imgur
+                    ...formData.getHeaders()
+                }
+            });
+
+            // Verificar si la subida de la imagen fue exitosa
+            if (imgurResponse.data.success) {
+                const image_url = imgurResponse.data.data.link;
+
+                // Construir el objeto del carro
+                const car = { license_plate, color, entryTime, state, image_url };
+                parkedCars.push(car);
+                console.log('Car parked:', car);
+                res.status(201).json(car);
+            } else {
+                res.status(500).json({ error: 'Error al subir la imagen al servidor de Imgur' });
+            }
+        });
+    } catch (error) {
+        console.error('Error al procesar la solicitud:', error);
+        res.status(500).json({ error: 'Error interno del servidor' });
+    }
 });
+
+
 
 
 // Listar los vehículos registrados
