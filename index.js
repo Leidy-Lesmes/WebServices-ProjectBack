06b565ef777993a4ae39462481c388f3d55252e8
Leidy-Lesmes <70681138+Leidy-Lesmes@users.noreply.github.com>
2024-02-27 08:51:13 -0500

    Validaciones de placas

--- index.js
+++ index.js
@@ -29,9 +29,15 @@ app.post('/cars', async (req, res) => {
     const { license_plate, color } = req.body;
     const entryTime = new Date();
     const state = "Activo";
-    let exitTime = null; // Inicializar exitTime como null
+    let exitTime = null; 
 
     try {
+
+        const existingCarIndex = parkedCars.findIndex(car => car.license_plate === license_plate);
+        if (existingCarIndex !== -1) {
+            return res.status(400).json({ error: 'Ya existe un carro con esta placa en el parqueadero.' });
+        }
+        
         if (!req.files || Object.keys(req.files).length === 0) {
             console.log(req.files);
             return res.status(400).send('No se encontró ningún archivo.');
@@ -42,7 +48,7 @@ app.post('/cars', async (req, res) => {
 
         // Guardar el archivo en el servidor
         file.mv(`${__dirname}/uploads/${fileName}`, async function(err) {
-            console.log ('El archivo se guardo en uploads: ' + fileName);
+            console.log ('El archivo se guardo en /uploads: ' + fileName);
             if (err) {
                 console.error('Error al guardar el archivo:', err);
                 return res.status(500).json({ error: 'Error interno del servidor al guardar el archivo.' });
@@ -65,12 +71,14 @@ app.post('/cars', async (req, res) => {
             // Verificar si la subida de la imagen fue exitosa
             if (imgurResponse.data.success) {
                 const image_url = imgurResponse.data.data.link;
+                console.log('El archivo se cargo correctamente al servidor de imagenes en el link  ' + image_url);
 
                 // Construir el objeto del carro
                 const car = { license_plate, color, entryTime, state, exitTime, image_url }; // Agregar exitTime
                 parkedCars.push(car);
                 console.log('Car parked:', car);
                 res.status(201).json(car);
+                
             } else {
                 res.status(500).json({ error: 'Error al subir la imagen al servidor de Imgur' });
             }
@@ -88,6 +96,8 @@ app.get('/cars', (req, res) => {
 });
 
 app.listen(PORT, () => {
+    const currentTime = new Date().toLocaleString();
+    console.log('Server start at:', currentTime);
     console.log(`Server running at http://${IP}:${PORT}/`);
 });
 
@@ -96,8 +106,11 @@ app.patch('/cars', (req, res) => {
     const { license_plate } = req.body;
     const index = parkedCars.findIndex(car => car.license_plate === license_plate);
     if (index !== -1) {
-        parkedCars[index].state = "Retirado"; // Cambiar el estado a "retirado"
-        parkedCars[index].exitTime = new Date(); // Agregar la hora de salida del carro
+        if (parkedCars[index].state === "Retirado") {
+            return res.status(400).json({ error: 'Este carro ya ha sido retirado y no puede ser retirado nuevamente.' });
+        }
+        parkedCars[index].state = "Retirado"; 
+        parkedCars[index].exitTime = new Date(); 
         const updatedCar = parkedCars[index];
         console.log('Car state updated:', updatedCar);
         res.json(updatedCar);
@@ -114,10 +127,8 @@ app.get('/cars/license-plates', (req, res) => {
             return res.status(404).json({ error: 'No hay carros registrados' });
         }
         
-        // Filtrar los carros en estado activo
         const activeCars = parkedCars.filter(car => car.state === 'Activo');
 
-        // Obtener las placas de los carros en estado activo
         const licensePlates = activeCars.map(car => car.license_plate);
 
         res.json(licensePlates);
